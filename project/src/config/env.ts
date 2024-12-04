aa0a9d0366f406ab95464af37d45ac0f2c39ea1c
SSShubhamRai <zzr79284@gmail.com>
2024-12-04 19:21:01 +0530

    adding new api key

--- project/src/config/env.ts
+++ project/src/config/env.ts
@@ -1,6 +1,6 @@
 // Environment configuration
 export const config = {
   xai: {
-    apiKey: 'xai-sXEpy2yGjgJh9QD9dC1o433zqRDldrVFOsnnY3GigtZwdAWkcLTsrlyKv7e13GTGrDfoAI2hBW6AA1yK'
+    apiKey: 'xai-o51axPJxjJmMz0EhAf1NqlrxLFyQ34ta2DJZC2uoub05p0QK4XVFr2BRX5PFaAO8ywyGotnyd9uweloC'
   }
 };
